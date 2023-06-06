35aa9ced83967371e9fac0f779ca78c7e9d78fd5
ongzl <zileong.ong@silverlakeaxis.com>
2023-06-07 02:19:26 +0800

    feat: implement homepage features updated

--- MyShop/src/app/services/auth.service.ts
+++ MyShop/src/app/services/auth.service.ts
@@ -5,6 +5,18 @@ import { AUTH_LOGIN_URL } from "../utils/constants";
 	providedIn: "root",
 })
 export class AuthService {
+	async fetchWithAuth(url: string, options: RequestInit = {}): Promise<Response> {
+		const token = sessionStorage.getItem("token");
+
+		options.headers = {
+			...options.headers,
+			"Content-Type": "application/json",
+			Authorization: `Bearer ${token}`,
+		};
+
+		return await fetch(url, options);
+	}
+
 	async loginAuthToken(username: string, password: string): Promise<any> {
 		const response = await fetch(AUTH_LOGIN_URL, {
 			method: "POST",
