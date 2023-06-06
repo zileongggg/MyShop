a891a43c385d1d78e6479ac9c109408fc53ddb62
ongzl <zileong.ong@silverlakeaxis.com>
2023-06-07 00:40:05 +0800

    Dependency Setup

---
+++ MyShop/src/app/app.module.ts
@@ -0,0 +1,49 @@
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { FormsModule } from "@angular/forms";
+
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatButtonModule } from "@angular/material/button";
+import { MatMenuModule } from "@angular/material/menu";
+import { MatGridListModule } from "@angular/material/grid-list";
+import { MatCardModule } from "@angular/material/card";
+import { MatIconModule } from "@angular/material/icon";
+import { MatListModule } from "@angular/material/list";
+import { MatTableModule } from "@angular/material/table";
+import { MatBadgeModule } from "@angular/material/badge";
+import { MatSnackBarModule } from "@angular/material/snack-bar";
+import { MatChipsModule } from "@angular/material/chips";
+import { MatPaginatorModule } from "@angular/material/paginator";
+import { MatInputModule } from "@angular/material/input";
+import { MatFormFieldModule } from "@angular/material/form-field";
+
+@NgModule({
+	declarations: [AppComponent],
+	imports: [
+		BrowserModule,
+		AppRoutingModule,
+		FormsModule,
+		BrowserAnimationsModule,
+		MatToolbarModule,
+		MatButtonModule,
+		MatGridListModule,
+		MatMenuModule,
+		MatCardModule,
+		MatIconModule,
+		MatListModule,
+		MatTableModule,
+		MatBadgeModule,
+		MatSnackBarModule,
+		MatChipsModule,
+		MatPaginatorModule,
+		MatInputModule,
+		MatFormFieldModule,
+	],
+	providers: [],
+	bootstrap: [AppComponent],
+})
+export class AppModule {}
